60fc879cb6cdb86a04d5145af5ef84c9bed5ed88
Andrew Harris <andrew@noblesamurai.com>
2015-05-06 17:34:51 +1000

    add border widths and colour options

--- index.js
+++ index.js
@@ -15,18 +15,28 @@ module.exports = function processImage(imageData, type, opts, callback) {
     var width = opts.width,
         height = opts.height,
         pad = opts.pad || 0,
-        colour = opts.colour || image.getPixel(0, 0);
+        colour = opts.colour || image.getPixel(0, 0),
+        borderWidths = opts.borderWidths || {},
+        borderColour = opts.borderColour || colour;
+
+    // construct inner width and height excluding borders
+    var innerWidth = width - (borderWidths.left || 0) - (borderWidths.right || 0),
+        innerHeight = height - (borderWidths.top || 0) - (borderWidths.bottom || 0);
 
     image.batch().
       // Resize image while maintaining aspect ratio.
       // We want to fix it within a square given by the height of the banner,
       // less any padding.
-      contain(height - (pad * 2), height - (pad * 2), colour).
+      contain(innerHeight - (pad * 2), innerHeight - (pad * 2), colour).
       // Now add the padding around the original image so it's not hard against
       // the edge of the banner.
       pad(pad, pad, pad, pad, colour).
       // Now pad out the LHS by width - height, in order to get the full width.
-      pad(width - height, 0, 0, 0, colour).
+      pad(innerWidth - innerHeight, 0, 0, 0, colour).
+      // Now add the border
+      pad(borderWidths.left || 0, borderWidths.top || 0,
+          borderWidths.right || 0, borderWidths.bottom || 0,
+          borderColour).
       // Excute the batched operations.
       exec(handleTransparency);
 
